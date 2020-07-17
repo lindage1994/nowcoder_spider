1d36e5aeddc76402027219d617e1a9b6b688a5f3
lindage1994 <lindage1994@126.com>
2020-07-17 17:03:10 +0700

    调整文件结构

--- experience.py
+++ experience.py
@@ -6,8 +6,11 @@ import codecs
 import re
 
 urlbase = "https://www.nowcoder.com"
-f = codecs.open('./notes/experience_new.md','a+','utf-8')
-def getIndexPage(url):
+# 总览目录
+overview = codecs.open('./overview.md','a+','utf-8')
+# 页面链接
+url_list = codecs.open('./list.md','a+','utf-8')
+def getIndexPage():
     driver = webdriver.Chrome()
     #这个是牛客网java实习面经的界面，可以根据自己的需要，找好网页，然后将连接复制到这里来
     driver.get("https://www.nowcoder.com/discuss/experience?tagId=639&order=3&companyId=0&phaseId=1")
@@ -46,6 +49,19 @@ def getUrl(page):
     list=[]
     for ul in soup.select(".js-nc-wrap-link"):
         list.append(ul.attrs['data-href'])
+        url_list.write(ul.attrs['data-href'] + '\n')
+        name = ul.span.get_text()
+        desc = ul.p.get_text()
+        overview.write('- [' + name + '](.' + ul.attrs['data-href'] + ') \n')
+    overview.close()
+    url_list.close()
+    return list
+    
+
+def readUrlFile():
+    url_list = codecs.open('./list.md','r','utf-8')
+    list = url_list.readlines()
+    url_list.close()
     return list
     
 
@@ -60,7 +76,8 @@ def getPageDetail(urll):
         return None
         
         
-def parseDetail(page):
+def parseDetail(url, page):
+    f = codecs.open('.' + url + '.md','a+','utf-8')
     soup = BeautifulSoup(page, 'lxml')
     title = soup.select(".discuss-title")[0].get_text().replace('\n','')
     author = soup.select(".js-nc-card")[1].get_text()
@@ -71,20 +88,21 @@ def parseDetail(page):
     f.write('### post-time \n' + post_time)
     f.write('### content \n')
     f.write(content.prettify())
+    f.close()
     
 
 def main():
-    page = getIndexPage("https://www.nowcoder.com/discuss/experience?tagId=639&order=3&companyId=0&phaseId=2")
+    page = getIndexPage()
     list = getUrl(page)
     print("一共有%d篇"%len(list))
     count = 0
     for item in list:
         content = getPageDetail(urlbase+item)
-        parseDetail(content)
+        parseDetail(item, content)
         #pageSave(str,count)
         count = count + 1
         print("进行到第%d篇了,Url为:" % count+urlbase+item)
-    f.close()
+
 
 
 
